70dd5fd26883254b2df1c877a22b5adae47d7d5d
jyolsna <isac.jyolsnaj@gmail.com>
2025-09-28 01:11:58 +0530

    Fix test issue for deployement workaround

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const logger = createLogger('server');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(helmet());
